4c08b6dbea9682ea72512eb5df10e82b8ae18f0a
Eduardo Alvarez <eduardo.alvarezr@elektra.com.mx-at-206757627403>
2022-09-23 10:14:44 -0500

    Se agregan
    -modelos para consumo

---
+++ app/src/main/java/com/example/readbitso/models/trading/PayloadTrades.kt
@@ -0,0 +1,11 @@
+package com.example.readbitso.models.trading
+
+data class PayloadTrades(
+    val amount: String,
+    val book: String,
+    val maker_side: String,
+    val price: String
+)
+
+
+
