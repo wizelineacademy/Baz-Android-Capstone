782fe2edab619f3471a7e7a1b97e641209abcbd1
Ari Valencia <ari.valencia@elektra.com.mx>
2022-09-27 11:03:20 -0500

    Âimprove User-Agent

--- core/src/main/java/com/ari/core/client/HeadersInterceptor.kt
+++ core/src/main/java/com/ari/core/client/HeadersInterceptor.kt
@@ -7,12 +7,15 @@ import okhttp3.Request
 import okhttp3.Response
 
 class HeadersInterceptor: Interceptor {
+
+    companion object {
+        private val USER_AGENT: String =
+            "Manufacturer: ${Build.MANUFACTURER} Model: ${Build.MODEL} Hardware: ${Build.HARDWARE} Android_OS: ${Build.VERSION.RELEASE}}"
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request().newBuilder()
-            .header("User-Agent", BuildConfig.LIBRARY_PACKAGE_NAME)
-            .header("Device-Manufacturer", Build.MANUFACTURER)
-            .header("Device-Model", Build.MODEL)
-            .header("Device-Hardware", Build.HARDWARE)
+            .header("User-Agent", USER_AGENT)
             .build()
 
         return chain.proceed(request)
