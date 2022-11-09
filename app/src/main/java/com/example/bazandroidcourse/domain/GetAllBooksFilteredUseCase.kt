5885a6b9fe03aa31181ac039fadcbe3e20f01f2c
JoseFlores-baz <jose.flores@elektra.com.mx>
2022-11-09 10:23:10 -0600

    use cases refactor

--- app/src/main/java/com/example/bazandroidcourse/domain/GetAllBooksFilteredUseCase.kt
+++ app/src/main/java/com/example/bazandroidcourse/domain/GetAllBooksFilteredUseCase.kt
@@ -1,6 +1,10 @@
 package com.example.bazandroidcourse.domain
 
+
+import com.example.bazandroidcourse.data.entities.BookModel
+import com.example.bazandroidcourse.data.entities.static.ApplicationCurrencies
 import com.example.bazandroidcourse.data.repository.BooksRepositoryInterface
+
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -9,7 +13,11 @@ class GetAllBooksFilteredUseCase(
     private val repository: BooksRepositoryInterface,
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
-    suspend operator fun invoke(currency: String) = withContext(defaultDispatcher){
-        repository.getAllBooks().filter{ it.book.endsWith("_$currency") }
-    }
+    suspend operator fun invoke(currency: String): List<BookModel> =
+        withContext(defaultDispatcher) {
+            repository.getAllBooks().filter {
+                it.book.endsWith("_$currency") &&
+                        ApplicationCurrencies.findByTicker((it.book)) != null
+            }
+        }
 }
